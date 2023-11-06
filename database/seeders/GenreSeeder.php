81fb8085caebf040a084b69ba40bdbd665c5fad3
Сергей Лотоцкий <serstel@gmail.com>
2023-11-07 00:37:48 +0600

    Genre seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Genre;
 
 class GenreSeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $genres = [
+            'Народная музыка',
+            'Духовная музыка',
+            'Академическая музыка',
+            'Популярная музыка',
+            'Фолк-музыка',
+            'Кантри',
+            'Латиноамериканская музыка',
+            'Блюз',
+            'Ритм-н-блюз',
+            'Джаз',
+            'Шансон, романс, авторская песня',
+            'Электронная музыка',
+            'Рок',
+            'Хип-хоп',
+            'Регги',
+            'Фанк',
+            'Новая волна',
+            'Соул',
+            'Диско',
+            'Поп-музыка',
+        ];
+
+        foreach ($genres as $genre) {
+            Genre::create(['name' => $genre]);
+        }
     }
 }
